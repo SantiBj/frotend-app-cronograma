a01cef3e2bdd679350ea65f0515f94f3fde59f50
Luibjs <luissantiagobeltranjimenez70@gmail.com>
2023-03-21 17:10:56 -0500

    termine la parte de seleccione la titulada y inicie la de la ficha

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './tailwind.css'
-import { Login } from './pages/Login'
-import { Home } from './pages/Home'
-import { Navegacion } from './components/share/Navegacion'
-import { Footer } from './components/share/Footer'
-import { Program } from './pages/Program'
-import { AssignConnect } from './pages/AssignConnect'
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "./tailwind.css";
+import { Navegacion } from "./components/share/Navegacion";
+import { Footer } from "./components/share/Footer";
+import { Login } from "./pages/Login";
+import { Home } from "./pages/Home";
+import { AssignConnect } from "./pages/AssignConnect";
+import { Program } from "./pages/Program";
+import { Ficha } from "./pages/Ficha";
 
 function App() {
   return (
@@ -13,16 +14,16 @@ function App() {
       <div className="App">
         <Navegacion />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='login' element={<Login />}></Route>
-          <Route path='assign' element={<AssignConnect/>}>
-            <Route path='program' element={<Program/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />}></Route>
+          <Route path="/assign" element={<AssignConnect />}>
+            <Route path="program" element={<Program />} />
+            <Route path="ficha" element={<Ficha />} />
           </Route>
         </Routes>
-        <Footer />
       </div>
     </HashRouter>
-  )
+  );
 }
 
-export default App
+export default App;
