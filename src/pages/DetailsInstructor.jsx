045249d2ae2edf68b6db06592e6c780a1406bbff
= <luissantiagobeltranjimenez70@gmail.com>
2023-05-08 14:01:45 -0500

    se crearon los inputs y el boton para generar repostes

--- src/pages/DetailsInstructor.jsx
+++ src/pages/DetailsInstructor.jsx
@@ -6,16 +6,18 @@ import { Data404 } from "../components/share/Data404";
 import { Error403 } from "../components/share/Error403";
 import { ErrorGeneric } from "../components/share/ErrorGeneric";
 import { PageHeader } from "../components/share/PageHeader";
-import { AiFillDelete } from "react-icons/ai";
+import { BiUserX } from "react-icons/bi";
 import { Calendar } from "../components/share/calendar/Calendar";
 import { Modal } from "../components/share/Modal";
 import { ContentModal } from "../components/DetailsInstructor/ContentModal";
 import { API_URL } from "../config";
 import { Navigate } from "react-router-dom";
 import { auth } from "../context/auth";
+import "../index.css";
+import { InputsReport } from "../components/share/InputsReport";
 
 export function DetailsInstructor() {
-  const { user } = useContext(auth)
+  const { user } = useContext(auth);
   const { slog } = useParams();
   const [visible, setVisible] = useState(false);
   const handleClick = () => setVisible(!visible);
@@ -30,7 +32,7 @@ export function DetailsInstructor() {
   const header = {
     headers: {
       "Content-type": "application/json; charset=UTF-8",
-      Authorization: userStorage ?  "token " + userStorage.token : null,
+      Authorization: userStorage ? "token " + userStorage.token : null,
     },
   };
 
@@ -48,9 +50,8 @@ export function DetailsInstructor() {
     consult();
   }, []);
 
-
-  if (!user || user && !user.isAdmin ){
-    return <Navigate to="/login"/>
+  if (!user || (user && !user.isAdmin)) {
+    return <Navigate to="/login" />;
   }
   if (loading || loadingIns) {
     return <Loading />;
@@ -74,17 +75,24 @@ export function DetailsInstructor() {
         />
       </Modal>
       <div className="w-[80%] max-w-[1200px] mx-auto">
-        <div className="my-[30px] flex justify-between items-center">
+        <div className="my-[30px] flex justify-between items-start">
           <PageHeader
             name={instructor.nombreCompleto}
             id={slog}
             instructor={true}
           />
           {instructor.documento !== user.documento && (
-            <div className="text-Red duration-300 hover:text-Black cursor-pointer">
-              <AiFillDelete onClick={handleClick} size={25} />
+            <div className="deleteInst rounded-full border-[2px] p-[2px] md:p-[5px] text-Red duration-300 hover:text-White hover:bg-Red cursor-pointer">
+              <BiUserX onClick={handleClick} size={30} />
             </div>
           )}
+        </div>
+        <div className="my-[50px] w-[60%] ">
+          <InputsReport>
+            <button className="w-full hover:bg-Green hover:text-White py-[8px] px-[5px] rounded-md font-medium duration-[300ms] bg-White border-Green border-[2px] text-Green">
+              Generar Reporte
+            </button>
+          </InputsReport>
         </div>
         <Calendar events={data} />
       </div>
